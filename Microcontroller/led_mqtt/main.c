a52e93ca06f4084a4735010d4f42a3dce1887d47
koppubalaji <137815588+koppubalaji@users.noreply.github.com>
2024-02-18 22:57:44 +0530

    Add files via upload

---
+++ Microcontroller/led_mqtt/main.c
@@ -0,0 +1,206 @@
+#include "main.h"
+#include"stdio.h"
+#include"string.h"
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
+void WE10_Init (char *, char *);
+void MQTT_Init(void);
+void mqtt_data_send(void);
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
+  WE10_Init("OPPO","12345678");
+  MQTT_Init();
+  while (1)
+  {
+	  mqtt_data_send();
+	  HAL_Delay(100);
+  }
+}
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+static void MX_USART1_UART_Init(void)
+{
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 38400;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+static void MX_USART2_UART_Init(void)
+{
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 38400;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+}
+void WE10_Init (char *SSID, char *PASSWD)
+{
+	char buffer[128];
+	sprintf (&buffer[0], "CMD+RESET\r\n");
+	HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
+
+	HAL_UART_Receive(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
+
+	sprintf (&buffer[0], "CMD+WIFIMODE=1\r\n");
+	HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
+
+	HAL_UART_Receive(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
+
+	sprintf (&buffer[0], "CMD+CONTOAP=OPPO,12345678\r\n");
+	HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), 10000);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 10000);
+	HAL_Delay(2000);
+	HAL_UART_Receive(&huart1, (uint8_t*)buffer, strlen(buffer), 10000);
+	HAL_Delay(500);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 10000);
+
+	sprintf (&buffer[0], "CMD?WIFI\r\n");
+	HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), 10000);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 10000);
+	HAL_UART_Receive(&huart1, (uint8_t*)buffer, strlen(buffer), 10000);
+	HAL_Delay(500);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 10000);
+}
+
+void MQTT_Init()
+{
+
+	char buffer[128];
+	sprintf (&buffer[0], "CMD+MQTTNETCFG=dev.rightech.io,1883\r\n");
+	HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), 10000);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 10000);
+	HAL_UART_Receive(&huart1, (uint8_t*)buffer, strlen(buffer), 10000);
+	HAL_Delay(500);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 10000);
+
+
+	sprintf (&buffer[0], "CMD+MQTTCONCFG=3,mqtt-koppubalaji-wduyjg,,,,,,,,,\r\n");
+	HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_UART_Receive(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_Delay(500);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
+
+
+	sprintf (&buffer[0], "CMD+MQTTSTART=1\r\n");
+	HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_Delay(5000);
+	HAL_UART_Receive(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_Delay(500);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
+
+	sprintf (&buffer[0], "CMD+MQTTSUB=base/relay/led1\r\n");
+	HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_Delay(500);
+	HAL_UART_Receive(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
+
+}
+
+void mqtt_data_send()
+{
+	char buffer[] = "EVT+SUBDATA=base/relay/led\r\n";
+	HAL_UART_Receive(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+	HAL_Delay(400);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
+	for(int i=0 ;i<=sizeof(buffer); i++)
+	{
+		if(buffer[i] == 'N')
+			{
+				HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+			}
+			else if(buffer[i]=='F')
+			{
+				HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+			}
+	}
+}
+void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+}
+#endif
